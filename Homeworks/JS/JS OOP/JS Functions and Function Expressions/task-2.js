fac8267d9b416f5af505ba247b0ca88f0b057d09
Emil Nik <emil.nik@abv.bg>
2015-06-19 02:13:25 +0300

    JS changes
    small changes

--- Homeworks/JS/JS OOP/JS Functions and Function Expressions/task-2.js
+++ Homeworks/JS/JS OOP/JS Functions and Function Expressions/task-2.js
@@ -7,31 +7,35 @@
 */
 
 function solve(x, y) {
+var result = [],
+        i,
+        j,
+        maxDivisor,
+        isPrime;
+    x = +x;
+    y = +y;
+
     if (arguments.length < 2) {
         throw new Error;
     } else if (isNaN(arguments[0]) || isNaN(arguments[1])) {
         throw new Error;
     }
-    var result = [],
-        i,
-        j,
-        maxDivisor,
-        isPrime;
-    x *= 1;
-    y *= 1;
 
     for (i = x; i <= y; i += 1) {
         maxDivisor = Math.sqrt(i);
         isPrime = true;
+
         if (i < 2) {
             isPrime = false;
         }
+
         for (j = 2; j <= maxDivisor; j += 1) {
             if (!(i % j)) {
                 isPrime = false;
                 break;
             }
         }
+
         if (isPrime) {
             result.push(i);
         }
