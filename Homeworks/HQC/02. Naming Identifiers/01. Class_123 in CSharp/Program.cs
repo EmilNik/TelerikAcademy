a11badef71e2f2522b2e043c3450a2509d37a479
Emil Nik <emil.nik@abv.bg>
2015-07-04 20:07:01 +0300

    HQC Naming Identifiers
    HQC Naming Identifiers

---
+++ Homeworks/HQC/02. Naming Identifiers/01. Class_123 in CSharp/Program.cs
@@ -0,0 +1,23 @@
+﻿//Refactor the following examples to produce code with well-named C# identifiers.
+
+using System;
+
+class class_123
+{
+    const int MAX_COUNT = 6;
+
+    class Converter
+    {
+        public void BoolToString(bool value)
+        {
+            string valueAsString = value.ToString();
+            Console.WriteLine(valueAsString);
+        }
+    }
+
+    public static void EntryPoint()
+    {
+        Converter converter = new Converter();
+        converter.BoolToString(true);
+    }
+}
---
+++ Homeworks/HQC/02. Naming Identifiers/02. Make_Чуек in CSharp/Program.cs
@@ -0,0 +1,33 @@
+﻿//Refactor the following examples to produce code with well-named C# identifiers.
+
+class Hauptklasse
+{
+    enum Gender
+    {
+        male,
+        female
+    };
+
+    class Person
+    {
+        public Gender Gender { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+    public void makeNewPerson(int personId)
+    {
+        Person currentPerson = new Person();
+        currentPerson.Age = personId;
+
+        if (personId % 2 == 0)
+        {
+            currentPerson.Name = "Батката";
+            currentPerson.Gender = Gender.male;
+        }
+        else
+        {
+            currentPerson.Name = "Мацето";
+            currentPerson.Gender = Gender.female;
+        }
+    }
+}
