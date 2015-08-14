991394d02f8c68a8370027afbc7dad97f824c321
Emil Nik <emil.nik@abv.bg>
2015-08-14 23:45:57 +0300

    HQC Refactoring Homework
    HQC Refactoring Homework

---
+++ Homeworks/HQC/HQC Refactoring/Main.cs
@@ -0,0 +1,16 @@
+﻿namespace TraverseMatrix
+{
+    using System;
+
+    public class Main
+    {
+        public static void Main()
+        {
+            Console.Write("Enter a number: ");
+            int value = int.Parse(Console.ReadLine());
+
+            Matrix matrix = new Matrix(value);
+            Console.WriteLine(matrix);
+        }
+    }
+}
