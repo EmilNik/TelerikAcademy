d24e2eb99fd23c22b19dfecd9e361442dff0f266
Emil Nik <emil.nik@abv.bg>
2015-08-24 02:45:05 +0300

    JS UnderscoreJs Homework
    JS UnderscoreJs Homework

--- Homeworks/JS/JS Applications/JS UnderscoreJS/task-6.js
+++ Homeworks/JS/JS Applications/JS UnderscoreJS/task-6.js
@@ -11,8 +11,39 @@ Create a function that:
 */
 
 function solve(){
-  return function (books) {
-  };
+    return function (books) {
+        var authors = [],
+            maxNumberOfBooks = 1;
+        _.chain(books)
+            .map(function(item){
+                item.author.authorName = item.author.firstName + ' ' + item.author.lastName;
+                return item;
+            })
+            .sortBy('authorName')
+            .groupBy('authorName')
+            .each(function(value, key){
+                authors.push([key, value.length]);
+            })
+            
+            authors = _.chain(authors)
+                .sortBy(function(item){
+                    return item[0];
+                })
+            .sortBy(function(item){
+                return -item[1];
+            })
+            .value();
+        
+        maxNumberOfBooks = authors[0][1];
+        
+        _.chain(authors)
+            .filter(function(item){
+                return item[1] === maxNumberOfBooks;
+            })
+            .each(function(item){
+                console.log(item[0]);
+            });
+    };
 }
 
 module.exports = solve;
