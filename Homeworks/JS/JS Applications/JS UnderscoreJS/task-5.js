cc5f5d9acf17274dfde5245083baaeef0b8f0393
Emil Nik <emil.nik@abv.bg>
2015-08-24 02:19:17 +0300

    JS UnderscoreJs Homework
    JS UnderscoreJs Homework

--- Homeworks/JS/JS Applications/JS UnderscoreJS/task-5.js
+++ Homeworks/JS/JS Applications/JS UnderscoreJS/task-5.js
@@ -8,15 +8,15 @@ Create a function that:
 */
 
 function solve(){
-  return function (animals) {
-    var totalNumberOfLegs = 0;
+    return function (animals) {
+        var totalNumberOfLegs = 0;
     
-    _.each(animals, function(item) {
-      totalNumberOfLegs += item.legsCount;
-    });
+        _.each(animals, function(item) {
+            totalNumberOfLegs += item.legsCount;
+        });
     
-    console.log('Total number of legs: ' + totalNumberOfLegs);
-  };
+        console.log('Total number of legs: ' + totalNumberOfLegs);
+    };
 }
 
 module.exports = solve;
