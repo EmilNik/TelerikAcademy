17601aedf4743e307f94dc3e27485123195c4c2a
Emil Nik <emil.nik@abv.bg>
2015-10-06 22:43:40 +0300

    XML Processing in .NET Homework
    XML Processing in .NET Homework

---
+++ Homeworks/Database/XML Processing in.NET/01. ExtractingArtistsFromCatalog/ExtractArtists.cs
@@ -0,0 +1,62 @@
+﻿namespace ExtractArtistsFromCatalog
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Xml;
+
+    internal class ExtractArtists
+    {
+        private static void Main()
+        {
+            var xmlDoc = new XmlDocument();
+            xmlDoc.Load("../../../catalog.xml");
+            
+            var rootNode = xmlDoc.DocumentElement;
+            
+            var artists = ExtractAllArtists(rootNode);
+            Console.WriteLine("All artists in the catalog:\n{0}", string.Join(", ", artists));
+
+            Console.WriteLine(new string('-', 70));
+            
+            var albums = ExtractNumberOfAlbumsForEachArtist(rootNode);
+            foreach (var album in albums)
+            {
+                Console.WriteLine("{0}: {1} album(s)", album.Key, album.Value);
+            }
+
+            Console.WriteLine(new string('-', 70));
+        }
+
+        private static IDictionary<string, int> ExtractNumberOfAlbumsForEachArtist(XmlElement rootNode)
+        {
+            var output = new Dictionary<string, int>();
+
+            var albums = rootNode.GetElementsByTagName("album");
+            foreach (XmlNode album in albums)
+            {
+                var artist = album["artist"].InnerText;
+                if (!output.ContainsKey(artist))
+                {
+                    output[artist] = 0;
+                }
+
+                output[artist]++;
+            }
+
+            return output;
+        }
+
+        private static IEnumerable<string> ExtractAllArtists(XmlElement rootNode)
+        {
+            var output = new HashSet<string>();
+
+            var albums = rootNode.GetElementsByTagName("album");
+            foreach (XmlNode album in albums)
+            {
+                output.Add(album["artist"].InnerText);
+            }
+
+            return output;
+        }
+    }
+}
---
+++ Homeworks/Database/XML Processing in.NET/02. ExtractingArtistsUsingXPath/ExtractingArtists.cs
@@ -0,0 +1,52 @@
+﻿namespace ExtractArtistsUsingXPath
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Xml;
+    
+    class ExxtractingArtistsUsingXPath
+    {
+        private static void Main()
+        {
+            const string xPathQuery = "/catalog/album";
+
+            var xmlDoc = new XmlDocument();
+            xmlDoc.Load("../../../catalog.xml");
+
+            var albumsList = xmlDoc.SelectNodes(xPathQuery);
+            var artistsList = ExtractArtistsFromCatalog(albumsList);
+
+            PrintArtists(artistsList);
+        }
+
+        private static void PrintArtists(Dictionary<string, int> artistsList)
+        {
+            foreach (var artist in artistsList)
+            {
+                Console.WriteLine("{0}: {1} album(s)", artist.Key, artist.Value);
+            }
+        }
+
+        private static Dictionary<string, int> ExtractArtistsFromCatalog(IEnumerable albumsList)
+        {
+            var artistsList = new Dictionary<string, int>();
+            foreach (XmlNode album in albumsList)
+            {
+                var artistName = album.SelectSingleNode("artist").InnerText;
+                if (!artistsList.ContainsKey(artistName))
+                {
+                    artistsList[artistName] = 0;
+                }
+
+                artistsList[artistName]++;
+            }
+
+            return artistsList;
+        }
+
+    }
+}
