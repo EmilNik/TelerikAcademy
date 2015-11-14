22f577a9e370d669d21347bd81a27e6d6523e020
Emil Nik <emil.nik1002@gmail.com>
2015-11-14 02:33:38 +0200

    StudetSystem

---
+++ Homeworks/WebServicesAndCloud/ASP.NET Web API/ASP.NET Web API/StudentSystem/StudentSystem.Services/Controllers/StudentsController.cs
@@ -0,0 +1,20 @@
+﻿namespace StudentSystem.Services.Controllers
+{
+    using System.Collections.Generic;
+    using System.Web.Http;
+
+    public class StudentsController : ApiController
+    {
+        // GET: api/students
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "student1", "student2" };
+        }
+
+        // GET: api/students/5
+        public string Get(int id)
+        {
+            return $"student {id}";
+        }
+    }
+}
