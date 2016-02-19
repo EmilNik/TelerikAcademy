83e7c02048814d52648f7e0925975b7c8da8449a
Emil Nik <emil.nik1002@gmail.com>
2016-02-19 17:54:00 +0200

    MVC Exam

--- Homeworks/ASP.NET/ASP.NET MVC/Exam/ASP.NET-MVC-JustAsk/Web/JustAsk.Web/Controllers/CommentsListController.cs
+++ Homeworks/ASP.NET/ASP.NET MVC/Exam/ASP.NET-MVC-JustAsk/Web/JustAsk.Web/Controllers/CommentsListController.cs
@@ -22,17 +22,19 @@
         public ActionResult Comments_Read([DataSourceRequest]DataSourceRequest request)
         {
             IQueryable<Comment> comments = this.db.Comments;
-            DataSourceResult result = comments.ToDataSourceResult(request, comment => new
-            {
-                Id = comment.Id,
-                Content = comment.Content,
-                AuthorEmail = comment.AuthorEmail,
-                AuthorId = comment.AuthorId,
-                CreatedOn = comment.CreatedOn,
-                ModifiedOn = comment.ModifiedOn,
-                IsDeleted = comment.IsDeleted,
-                DeletedOn = comment.DeletedOn
-            });
+            DataSourceResult result = comments
+                .ToDataSourceResult(request,
+                comment => new
+                {
+                    Id = comment.Id,
+                    Content = comment.Content,
+                    AuthorEmail = comment.AuthorEmail,
+                    AuthorId = comment.AuthorId,
+                    CreatedOn = comment.CreatedOn,
+                    ModifiedOn = comment.ModifiedOn,
+                    IsDeleted = comment.IsDeleted,
+                    DeletedOn = comment.DeletedOn
+                });
 
             return this.Json(result);
         }
